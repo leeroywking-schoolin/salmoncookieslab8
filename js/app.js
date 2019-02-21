15c11f5f4106eb2d800ef9bfed8ef71225e51306
Lee-Roy King <leeroywking@gmail.com>
2019-02-20 19:55:17 -0800

    added a function for making rows, plan is to call it immediately before adding data to the table row

--- js/app.js
+++ js/app.js
@@ -18,37 +18,38 @@ var hours = [
   '8:00pm'
 ];
 
+var tableMain = document.getElementById('tablemain');
+
+function createRow(rowID){
+  var newRow = document.createElement('tr');
+  newRow.id = rowID;
+  tableMain.appendChild(newRow);
+}
 
 var firstAndPike = document.getElementById('1andpike');
 var seaTac = document.getElementById('seatac');
 var seattleCenter = document.getElementById('seacenter');
 var caphill = document.getElementById('caphill');
 var alki = document.getElementById('alkibeach');
+
+
 var tablehead = document.getElementById('tablehead');
 var tablefoot = document.getElementById('rowtotals');
 
-var firstAndPikestaff = document.getElementById('1andpikestaff');
-var seaTacstaff = document.getElementById('seatacstaff');
-var seattleCenterstaff = document.getElementById('seacenterstaff');
-var caphillstaff = document.getElementById('caphillstaff');
-var alkistaff = document.getElementById('alkibeachstaff');
-
-// var tablehead = document.getElementById('tablehead');
 
 // You need to pass this constructor function the max, min and cookie per sale, also the htmlElement from above ie var locationObject = new LocationConstructor(100,30,3.5,html id));
-function LocationConstructor(max,min,cookiepersale,htmlElementId,staffID,rowTitle) {
+function LocationConstructor(max,min,cookiepersale,htmlElementId,rowTitle) {
   this.maxCustPerHour = max;
   this.minCustPerHour = min;
   this.cookiesPerSale = cookiepersale;
   this.rowTitle = rowTitle;
-  this.staffID = staffID;
   this.randRange = function(){
     return Math.floor((Math.random() *(this.maxCustPerHour - this.minCustPerHour)) + this.minCustPerHour)};
   this.cookiesPerHour= this.numCustomerPerHour * this.cookiesPerSale
   this.cookiesByHour= []
   this.employeesByHour = [] 
   this.render= function () {
-    var total = 0
+    var total = 0;
     var rowTitleRend = document.createElement('td');
     rowTitleRend.textContent = this.rowTitle;
     htmlElementId.appendChild(rowTitleRend);
@@ -66,39 +67,13 @@ function LocationConstructor(max,min,cookiepersale,htmlElementId,staffID,rowTitl
     this.cookiesByHour.push(total);
     htmlElementId.appendChild(liEltotal);
   }
-
-  this.employee= function(){
-   
-    var rowTitleRend = document.createElement('td');
-    rowTitleRend.textContent = this.rowTitle;
-    staffID.appendChild(rowTitleRend);
-    var highest = 0;
-    for (var i = 0; i < hours.length; i++) {
-      var liEl = document.createElement('td');
-      var customers = Math.floor(this.cookiesByHour[i] / this.cookiesPerSale);
-      function necessaryEmployees(){
-        if ((customers / 20) < 2) {return 2}
-        else {return Math.ceil(customers/20)}
-      }
-      var nec = necessaryEmployees()
-      this.employeesByHour.push(nec);
-      if (nec > highest){highest = nec}
-      else {};
-      liEl.textContent = `${nec} `;
-      staffID.appendChild(liEl);
-    }
-    var emptotal = document.createElement('td');
-    emptotal.textContent = `${highest} employees`;
-    this.employeesByHour.push(highest);
-    staffID.appendChild(emptotal);
-  }
 };
 
-var firstAPObject = new LocationConstructor(65,23,6.3,firstAndPike,firstAndPikestaff,'First and Pike');
-var seaTacObject = new LocationConstructor(24,2,1.2,seatac,seatacstaff, 'SeaTac Airport');
-var seaCentObject = new LocationConstructor(38,11,3.7,seacenter,seacenterstaff,'Seattle Center');
-var capHillObject = new LocationConstructor(38,20,2.3,caphill,caphillstaff,'Capitol Hill');
-var alkiBeachObject = new LocationConstructor(16,2,4.6,alkibeach,alkibeachstaff,'Alki');
+var firstAPObject = new LocationConstructor(65,23,6.3,firstAndPike,'First and Pike');
+var seaTacObject = new LocationConstructor(24,2,1.2,seatac, 'SeaTac Airport');
+var seaCentObject = new LocationConstructor(38,11,3.7,seacenter,'Seattle Center');
+var capHillObject = new LocationConstructor(38,20,2.3,caphill,'Capitol Hill');
+var alkiBeachObject = new LocationConstructor(16,2,4.6,alkibeach,'Alki');
 
 function tableHeadRender(headElement){
   var blank = document.createElement('th');
@@ -129,35 +104,66 @@ function tableFootRender(footElement){
   }
 }
 
-function tableFootstaffRender(footElement){
-  var label = document.createElement('td');
-  label.innerHTML = ('By Hour Totals');
-  footElement.appendChild(label);
-  for (var i= 0; i < hours.length +1; i++){
-    var entry = document.createElement('td');
-    entry.textContent = (firstAPObject.employeesByHour[i] + 
-      seaTacObject.employeesByHour[i] + 
-      seaCentObject.employeesByHour[i] + 
-      capHillObject.employeesByHour[i] +
-      alkiBeachObject.employeesByHour[i])
-      footElement.appendChild(entry)
+var objList = [firstAPObject,seaTacObject,seaCentObject,capHillObject,alkiBeachObject]
+
+function render(){
+  tableHeadRender(tablehead);
+
+  for (var i =0; i< objList.length; i ++){
+    objList[i].render();
   }
+  tableFootRender(rowtotals);
 }
-firstAPObject.render();
-seaTacObject.render();
-seaCentObject.render();
-capHillObject.render();
-alkiBeachObject.render();
 
-tableHeadRender(tablehead);
-tableFootRender(rowtotals);
-tableHeadRender(tableheadstaff);
+render();
+
+var chatList = document.getElementById('chat-list');
+var chatForm = document.getElementById('chat-form');
+var allComments = [];
+
+var Comment = function(max, min, avg, elementId, name) {
+  this.max = max;
+  this.min = min;
+  this.avg = avg;
+  this.elementId = elementId;
+  this.name = name;
+};
+
+Comment.prototype.render = function() {
+  var liEl = document.createElement('li');
+  // liEl.innerHtml = '<b>' + this.username + ': </b><em>' + this.text + '</em>';
+  liEl.innerHTML = ' <b>' + this.max + ': </b><em>' + this.min + '</em>' + this.avg + elementId + name;
+  return liEl;
+
+};
+
+function handleCommentSubmit(event) {
+  event.preventDefault();
+  var max = event.target.max.value;
+  var min = event.target.min.value;
+  var avg = event.target.avg.value;
+  var elementId = event.target.elementId.value;
+  var name = event.target.elementId.value;
 
 
-firstAPObject.employee();
-seaTacObject.employee();
-seaCentObject.employee();
-capHillObject.employee();
-alkiBeachObject.employee();
+  var newStore = new LocationConstructor(max, min, avg, elementId, name);
 
-tableFootstaffRender(rowtotalsstaff);
+  event.target.max.value = null;
+  event.target.min.value = null;
+  event.target.avg.value = null;
+  event.target.elementId.value = null;
+  event.target.name.value = null;
+
+  
+  allComments.unshift(newComment);
+  renderAllComments();
+}
+
+// function renderAllComments() {
+//   chatList.innerHTML = '';
+//   for(var i = 0; i < allComments.length; i++) {
+//     chatList.appendChild(allComments[i].render());
+//   }
+// }
+
+chatForm.addEventListener('submit', handleCommentSubmit);
